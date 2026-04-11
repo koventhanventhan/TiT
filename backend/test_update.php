5d38fd9ca2ccc9e46222475d28d1a64212731752
koventhanventhan <167792357+koventhanventhan@users.noreply.github.com>
2026-04-12 00:35:51 +0530

    Fix paths in password reset script

--- backend/test_update.php
+++ backend/test_update.php
@@ -5,8 +5,8 @@ use App\Models\User;
 
 define('LARAVEL_START', microtime(true));
 
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
+require __DIR__.'/../vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 $kernel->bootstrap();
